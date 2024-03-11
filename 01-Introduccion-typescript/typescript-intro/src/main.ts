155150063d32ed61243cb08d7e157ceaa2c233d0
Jorge Matos <jisapjisap@gmail.com>
2024-03-11 19:32:18 +0100

    Objetos

--- 01-Introduccion-typescript/typescript-intro/src/main.ts
+++ 01-Introduccion-typescript/typescript-intro/src/main.ts
@@ -1,14 +1,22 @@
 import './style.css'
 
-import { name, templateString } from '../bases/01-type';
-
+//import { name, templateString } from '../bases/01-type';
+import { charmander, bulbasaur, pokemons } from '../bases/02-objects';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
+// 01-types.ts
+
+// app.innerHTML = `
+//   <h1>Hello ${ name }</h1>
+//   </br>
+//   ${templateString}
+// `
+
+// 02-objects.ts
 app.innerHTML = `
-  <h1>Hello ${ name }</h1>
-  </br>
-  ${templateString}
-`
+  <h1>Hello ${charmander.name} ${charmander.id}!</h1>
+  <h1>Hello ${bulbasaur.name} ${bulbasaur.id}</h1>
+`;
 
 
