4ddcd7eb6fb5e8c2b82ba758a2b888a5c00d1a3a
Jorge Matos <jisapjisap@gmail.com>
2024-03-16 17:01:14 +0100

    controller

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')                   // Escucha las peticiones (a localhost:3000/cars) de los clientes (postman), y emite una respuesta
 export class CarsController {
 
+  private cars = ['Toyota', 'Honda', 'Jeep']
+
   @Get()
   getAllCars(){
-    return ['Toyota', 'Honda', 'Jeep']
+    return this.cars
+  }
+
+  @Get(':id')
+  getCarById( @Param('id') id:string ){
+    console.log({id})
+    const car = this.cars[id]
+    return {
+      id,
+      car
+    }
   }
 }
