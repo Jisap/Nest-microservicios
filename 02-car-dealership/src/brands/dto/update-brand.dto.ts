791eb4fd7235908c85d7b32ae6c8a60a8d5ab142
Jorge Matos <jisapjisap@gmail.com>
2024-03-21 20:15:35 +0100

    nest cli resource

--- 02-car-dealership/src/brands/dto/update-brand.dto.ts
+++ 02-car-dealership/src/brands/dto/update-brand.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/mapped-types'; // Permite extender un dto basado en otro dto
 import { CreateBrandDto } from './create-brand.dto';
 
-export class UpdateBrandDto extends PartialType(CreateBrandDto) {}
+export class UpdateBrandDto extends PartialType(CreateBrandDto) { // PartialType permite que las props del dto de donde se extiende seán opcionales
+
+}
