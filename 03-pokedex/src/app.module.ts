f9fad51344ab7d3ff0b17f5e947c44e3d111a957
Jorge Matos <jisapjisap@gmail.com>
2024-03-25 22:03:34 +0100

    conexion a mongodb

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,13 +1,25 @@
-import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { EnvConfiguration } from './config/app.config';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
   imports: [
+
+    ConfigModule.forRoot({
+      load: [EnvConfiguration]
+    }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+
+    MongooseModule.forRoot(process.env.MONGODB),
+
     PokemonModule,
   ],
   controllers: [],
