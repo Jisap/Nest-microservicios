26c7a1bc2c612b51650d17b70bd9150fb63b68d9
Jorge Matos <jisapjisap@gmail.com>
2024-04-02 22:06:20 +0200

    EnvConfiguration

--- 03-pokedex/src/config/app.config.ts
+++ 03-pokedex/src/config/app.config.ts
@@ -4,6 +4,7 @@
 
 export const EnvConfiguration = () => ({
   
+  environment: process.env.NODE_ENV || 'dev',
   mongodb: process.env.MONGODB,
   defaultLimit: process.env.DEFAULT_LIMIT,
   port: process.env.PORT,
