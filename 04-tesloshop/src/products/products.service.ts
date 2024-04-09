36436ead04a27ae2d90d46cb171eae4ac61071e5
Jorge Matos <jisapjisap@gmail.com>
2024-04-09 23:03:29 +0200

    insertar usando typeORM

--- 04-tesloshop/src/products/products.service.ts
+++ 04-tesloshop/src/products/products.service.ts
@@ -1,12 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+
+  constructor(                                                  // En el constructor indicamos que trabajamos con la entidad Product
+    @InjectRepository(Product) //Modelo                         // a traves de una inyección de un repositorio. Este repositorio nos permite
+    private readonly productRepository: Repository<Product>,    // trabajar (CRUD) con las instancias de las entidades de Product (productRepository)
+                    // Instancias de pto
+  ){}
   
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  async create(createProductDto: CreateProductDto) {
+    try {
+
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+
+      return product;
+
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException('Ayuda');
+    }
   }
 
   findAll() {
