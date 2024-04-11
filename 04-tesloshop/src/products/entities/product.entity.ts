0836b2d2929337e3fe9746a9f4903b58c04e22a5
Jorge Matos <jisapjisap@gmail.com>
2024-04-11 22:17:27 +0200

    BeforeInsert & BeforeUpdate

--- 04-tesloshop/src/products/entities/product.entity.ts
+++ 04-tesloshop/src/products/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: 'products' })
 export class Product {
@@ -39,4 +39,24 @@ export class Product {
 
   @Column('text')
   gender: string;
+
+  @BeforeInsert()                                                     // Antes de la inserción en la bd
+  checkSlugInsert() {                                                 // Llamamos a este método que verifica que  
+    if (!this.slug) {                                                 // sino existe el slug en el body
+      this.slug = this.title                                          // usamos el title como slug
+    }
+
+    this.slug = this.slug                                              // Si el slug existe entonces
+      .toLowerCase()                                                   // lo pasamos a minúsculas
+      .replaceAll(' ', '_')                                            // y sustituimos los espacios por _
+      .replaceAll("´", '')                                             // y eliminamos los ´
+  }
+
+  @BeforeUpdate()                                                      // Despues de la actualización en bd
+  checkSlugUpdate() {                                                  // Llamamos a este otro método que verifica que
+    this.slug = this.slug                                              // el slug existe
+      .toLowerCase()                                                   // lo pasamos a minúsculas
+      .replaceAll(' ', '_')                                            // sustituimos los espacios por _
+      .replaceAll("´", '')                                             // y eliminamos los ´
+  }
 }
