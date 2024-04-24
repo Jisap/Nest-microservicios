932c6afb89dfe66638d1708f8d675faeaa98d46e
Jorge Matos <jisapjisap@gmail.com>
2024-04-24 20:20:38 +0200

    uploadFiles

---
+++ 04-tesloshop/src/files/files.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
+
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')                                                    // Petición post a api/files/product para subida de un archivo
+  @UseInterceptors(FileInterceptor('file', {                          // Interceptamos la petición de subida de archivos para modificar la respuesta sobre el archivo 'file'
+    fileFilter: fileFilter,                                           // fileFilter recibe el file y evalua si tiene extensión permitida y si no se seleccion nada como file                                              
+  }))
+  uploadProductImage(                                                 // Esta función recibe el file validado por extensión, renombrado por uuid y almacenado en fs
+    @UploadedFile()                                                   // Este decorador recibe el archivo subido y permite obtener su metadata
+    file:Express.Multer.File
+  ){
+    if (!file) {                                                              // Si se recibio un false del fileFilter por extensión no aceptada 
+      throw new BadRequestException('Make sure that the file is an image')    // mensaje de error
+    }
+
+    return {
+      filename: file.originalname
+
+    }
+  }
+}
