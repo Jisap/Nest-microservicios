8ba201e49383392cf3891c0bcb56f84da246c2a4
Jorge Matos <jisapjisap@gmail.com>
2024-04-26 12:40:30 +0200

    logger

--- 04-tesloshop/src/main.ts
+++ 04-tesloshop/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
 
   app.setGlobalPrefix('api');           // Ruta ppal de la API.
   
@@ -15,6 +16,6 @@ async function bootstrap() {
 );
 
   await app.listen(process.env.PORT);
-  console.log(`App running on port ${ process.env.PORT }`)
+  logger.log(`App running on port ${process.env.PORT}`)
 }
 bootstrap();
