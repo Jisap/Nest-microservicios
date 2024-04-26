f8486faf3a2f645fe6920d220b604aa5506d6e8e
Jorge Matos <jisapjisap@gmail.com>
2024-04-26 11:23:33 +0200

    diskStorage $ filenamer

--- 04-tesloshop/src/files/files.controller.ts
+++ 04-tesloshop/src/files/files.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInt
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
+import { fileNamer } from './helpers/fileNamer.helpers';
 
 
 @Controller('files')
@@ -11,6 +13,10 @@ export class FilesController {
   @Post('product')                                                    // Petición post a api/files/product para subida de un archivo
   @UseInterceptors(FileInterceptor('file', {                          // Interceptamos la petición de subida de archivos para modificar la respuesta sobre el archivo 'file'
     fileFilter: fileFilter,                                           // fileFilter recibe el file y evalua si tiene extensión permitida y si no se seleccion nada como file                                              
+    storage: diskStorage({                                            // Si la extensión es permitida se procede a su almacenaj
+      destination: './static/products',                               // en la carpeta '/static/products' 
+      filename: fileNamer                                             // y su renombre del archivo con uuid
+    })
   }))
   uploadProductImage(                                                 // Esta función recibe el file validado por extensión, renombrado por uuid y almacenado en fs
     @UploadedFile()                                                   // Este decorador recibe el archivo subido y permite obtener su metadata
