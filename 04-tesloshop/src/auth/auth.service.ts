ee3d075e5bd5e3720b8feacc8bbb5e3151d73bf4
Jorge Matos <jisapjisap@gmail.com>
2024-04-28 18:36:03 +0200

    create user

--- 04-tesloshop/src/auth/auth.service.ts
+++ 04-tesloshop/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
@@ -22,11 +22,19 @@ export class AuthService {
       return user;
 
     } catch (error) {
-      console.log(error)
+      this.handleDBErrors(error)
     }
 
     return 'This action adds a new auth';
   }
 
-  
+  private handleDBErrors(error: any): never {
+
+    if (error.code === '23505')
+      throw new BadRequestException(error.detail)
+
+    console.log(error)
+
+    throw new InternalServerErrorException('PLease check server logs')
+  }
 }
