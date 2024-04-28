3e86a80faabf14f0cfab98095b88f1e4f11d0384
Jorge Matos <jisapjisap@gmail.com>
2024-04-28 18:23:24 +0200

    create user

--- 04-tesloshop/src/auth/auth.service.ts
+++ 04-tesloshop/src/auth/auth.service.ts
@@ -1,26 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
+
+  constructor(
+    @InjectRepository(User)                                 // El módulo AuthService trabaja con instancias del modelo User
+    private readonly userRepository: Repository<User>       // Las instancias son los userRepository basadas en la entity User
+  ){}
+
+  create(createAuthDto: CreateUserDto) {
     return 'This action adds a new auth';
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+  
 }
