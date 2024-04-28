100e9466675ef03ec42d211045f20e38c9fbff40
Jorge Matos <jisapjisap@gmail.com>
2024-04-28 18:34:12 +0200

    create user

--- 04-tesloshop/src/auth/auth.service.ts
+++ 04-tesloshop/src/auth/auth.service.ts
@@ -13,7 +13,18 @@ export class AuthService {
     private readonly userRepository: Repository<User>       // Las instancias son los userRepository basadas en la entity User
   ){}
 
-  create(createAuthDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto) {
+
+    try {
+      
+      const user = this.userRepository.create(createUserDto);
+      await this.userRepository.save( user );
+      return user;
+
+    } catch (error) {
+      console.log(error)
+    }
+
     return 'This action adds a new auth';
   }
 
