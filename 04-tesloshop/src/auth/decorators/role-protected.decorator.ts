869954e61e21c51956c374d6767a9e75285b2f60
Jorge Matos <jisapjisap@gmail.com>
2024-05-02 17:24:11 +0200

    custom decorator - roleProtected

--- 04-tesloshop/src/auth/decorators/role-protected.decorator.ts
+++ 04-tesloshop/src/auth/decorators/role-protected.decorator.ts
@@ -1,8 +1,9 @@
 import { SetMetadata } from '@nestjs/common';
+import { ValidRoles } from '../interface/valid-roles';
 
 export const META_ROLES = 'roles'
 
-export const RoleProtected = (...args: string[]) => { // Establece los roles permitidos para una ruta através de una constante 
+export const RoleProtected = (...args: ValidRoles[]) => { // Establece los roles permitidos para una ruta através de una constante 
 
   return SetMetadata( META_ROLES, args );
 }
