68686c9189b1ac47cda03f8d754b3d68cf73e560
Jorge Matos <jisapjisap@gmail.com>
2024-05-08 17:42:53 +0200

    Escuchar conexiones y desconexiones

--- 04-tesloshop/src/messages-ws/messages-ws.gateway.ts
+++ 04-tesloshop/src/messages-ws/messages-ws.gateway.ts
@@ -1,9 +1,20 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {  // Estas interfaces permiten saber cuando un usuario se conecta o desconecta
+  
+  constructor(
+    private readonly messagesWsService: MessagesWsService
+  ) {}
+
+  handleConnection( client: Socket ) {
+    console.log('cliente conectado: ', client.id)
+  }
+  handleDisconnect( client: Socket ) {
+    console.log('cliente desconectado: ', client.id)
+  }
 
   
 }
